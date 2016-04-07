051f90688c914885dcbd715522c9395a49dba6aa
Shadi Copty <shadi.copty@yahoo.com>
2016-04-06 20:01:36 -0700

    last update

--- plot3.R
+++ plot3.R
@@ -2,6 +2,11 @@ data <- read.csv(file = "household_power_consumption.txt", dec=".", sep=";", str
 smallerData <- data[data$Date=="1/2/2007" | data$Date=="2/2/2007",]
 ap <- as.numeric(smallerData$Global_active_power)
 datetime <- strptime(paste(smallerData$Date, smallerData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+sm1 <- as.numeric(smallerData$Sub_metering_1)
+sm2 <- as.numeric(smallerData$Sub_metering_2)
+sm3 <- as.numeric(smallerData$Sub_metering_3)
 png("plot3.png", width=480, height=480)
-plot(datetime, ap,main="Global Active Power over time", ylab="Global Active Power (kilowatts)", type="l")
+plot(datetime, sm1, ylab="Energy Submetering", type="l", main="sub metering")
+lines(datetime, sm2, type="l", col="red")
+lines(datetime, sm3, type="l", col="blue")
 dev.off()
